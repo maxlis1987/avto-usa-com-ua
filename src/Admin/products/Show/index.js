f66deab61322b8fb2b2d71f5d62cd39320751321
Maxim Lisevich <maxim.lisevich@gmail.com>
2019-05-01 01:01:13 +0300

    Updated phone numbers

--- src/Admin/products/Show/index.js
+++ src/Admin/products/Show/index.js
@@ -12,7 +12,7 @@ const ProductEdit = ({ classes, ...props }) => {
 			<SimpleForm toolbar={<PostShowActions />} className={classes.main} redirect="show" resource="userId">
 				<Paper className={classes.headerDescriptionLable}>
 					Все машины уже выкуплены и оплачены с аукционов и находиться в порту Одесса. <br /> По вопросам
-					оплаты или заказа автомобиля обращаться по тел. 0678323457 0982416505 0975993309
+					оплаты или заказа автомобиля обращаться по тел. 0672323457 0982416505 0975993309
 				</Paper>
 				<ResponsiveBlock {...props} />
 			</SimpleForm>
